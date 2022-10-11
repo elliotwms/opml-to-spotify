3541cc753e9170e2992f767694887007d09e4bb8
Elliot Williams <4396779+elliotwms@users.noreply.github.com>
2022-10-11 12:17:02 +0100

    test: don't actually call the iTunes Search API in tests

--- internal/clients/itunes/search_test.go
+++ internal/clients/itunes/search_test.go
@@ -1,9 +1,33 @@
 package itunes
 
-import "testing"
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	res, err := Search("James O'Brien's Mystery Hour", "GB")
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bs, _ := json.Marshal(Results{
+			ResultsCount: 1,
+			Results: []Entry{
+				{
+					TrackName: "James O'Brien's Mystery Hour",
+					FeedURL:   "rss",
+				},
+			},
+		})
+
+		_, _ = w.Write(bs)
+	}))
+
+	client := &Client{
+		HTTPClient: s.Client(),
+		BaseURL:    s.URL,
+	}
+
+	res, err := client.Search("James O'Brien's Mystery Hour", "GB")
 	if err != nil {
 		t.Fatal(err)
 	}
